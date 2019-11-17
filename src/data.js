ab94b929dc3de1f3b2a36f43742b9b656e59d7f8
Aj <Aj.alyounis@gmail.com>
2019-11-17 12:40:51 -0800

    add new layout

--- src/data.js
+++ src/data.js
@@ -1,30 +1,44 @@
 export default [
-	{
-		id: 1,
-		title: 'The Art Of War',
-		price: 10.11,
-		image:
-			'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg'
-	},
-	{
-		id: 2,
-		title: 'Civilization and Its Discontents',
-		price: 14.29,
-		image:
-			'https://images-na.ssl-images-amazon.com/images/I/51Jj12iMZnL._SX331_BO1,204,203,200_.jpg'
-	},
-	{
-		id: 3,
-		title: 'Influence: The Psychology of Persuasion',
-		price: 10.98,
-		image:
-			'https://images-na.ssl-images-amazon.com/images/I/512-B-1yXuL._SX331_BO1,204,203,200_.jpg'
-	},
-	{
-		id: 4,
-		title: 'Grokking Algorithms',
-		price: 40.00,
-		image:
-			'https://images-na.ssl-images-amazon.com/images/I/61uUPXbhMxL._SX397_BO1,204,203,200_.jpg'
-	}
+  {
+    id: 1,
+    title: "The Art Of War",
+    price: 10.11,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 2,
+    title: "Civilization and Its Discontents",
+    price: 14.29,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51Jj12iMZnL._SX331_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 3,
+    title: "Influence: The Psychology of Persuasion",
+    price: 10.98,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/512-B-1yXuL._SX331_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 4,
+    title: "Grokking Algorithms",
+    price: 40.0,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61uUPXbhMxL._SX397_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 5,
+    title: "World War",
+    price: 29.0,
+    image:
+      "http://artec3d-production.s3-eu-west-1.amazonaws.com/btx_news/1442698381_world-war-z-uk-poster.jpg"
+  },
+  {
+    id: 5,
+    title: "Mark Wahlberg",
+    price: 38.0,
+    image:
+      "https://i.pinimg.com/originals/f0/10/51/f01051b26c0f89d57517a9f1fd23aaf2.jpg"
+  }
 ];
